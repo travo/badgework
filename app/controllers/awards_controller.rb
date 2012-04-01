53b76afaaa084bd99405d2972eb92e21aced7a51
Travis Winters <travo@prozacblues.com>
2012-04-01 21:32:16 +1000

    Let's shove some stuff around for twitter bootstrap and see how it looks.

--- app/controllers/awards_controller.rb
+++ app/controllers/awards_controller.rb
@@ -3,8 +3,13 @@ class AwardsController < ApplicationController
   inherit_resources
 
   before_filter :find_award, :only => :show
+  before_filter :find_awards
 
   private
+
+  def find_awards
+    @awards = Award.all
+  end
 
   def find_award
     @award = Award.find_using_slug(params[:id])
