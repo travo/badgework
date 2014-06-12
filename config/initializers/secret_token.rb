89f7b667ad4a1ef74b4af018d979eeb31b123976
Travis Winters <travo@prozacblues.com>
2014-06-12 21:09:42 +1000

    Seatec Astronomy

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,7 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Badgework::Application.config.secret_token = 'b9ee5130c803dfac5538c9e3517b5f6fec5d355f9bb2c01d311a93cdeac00e3dfe0f1d72393a5de952fe5e07832052233c048d303307c95fe6ca12d1d05aaa4a'
+token = '—— I AM A SECRET CHANGE ME ——'
+
+Badgework::Application.config.secret_token = token
+Badgework::Application.config.secret_key_base = token
